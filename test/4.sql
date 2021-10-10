8f02719655968d96621463573ce923d07accb9ab
LaFrance Daniels III <55370968+LDaniels18@users.noreply.github.com>
2021-10-09 17:46:17 -0700

    changes

--- test/4.sql
+++ test/4.sql
@@ -1,6 +1,8 @@
 -- How  many  parts  with  size  below 20 does  every  supplier  from CANADA offer?   
 -- Print  the  name  of  thesupplier and the number of parts.
-
-SELECT s_name, count(p_type)
-FROM part
-
+select s_name, count(p_partkey) from nation
+inner join part on p_partkey is ps_partkey
+inner join partsupp on ps_suppkey is s_suppkey
+inner join supplier on s_nationkey is n_nationkey
+where p_size < 20 and n_name is 'CANADA'
+group by s_name;
