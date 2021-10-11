0e1be20a8a3955eb1841796720f9374de309cfcc
LaFrance Daniels III <55370968+LDaniels18@users.noreply.github.com>
2021-10-10 19:22:29 -0700

    9 done

--- lab-5/test/9.sql
+++ lab-5/test/9.sql
@@ -1,10 +1,33 @@
---test
--- select p_name from part, nation
--- inner join partsupp on ps_partkey is p_partkey
--- where n_name is 'UNITED STATES'
--- group by p_name
--- limit(ps_supplycost * ps_availqty);
+--test -- need to finish
+ select p_name from part
+ inner join supplier on s_suppkey = ps_suppkey
+ inner join partsupp on ps_partkey = p_partkey
+ INNER JOIN nation on n_nationkey = s_nationkey
+ where n_name is 'UNITED STATES'
+ 
 
 
-select (ps_supplycost * ps_availqty) as total from partsupp
-limit 1;
+
+
+
+ group by p_name
+ HAVING (ps_supplycost * ps_availqty) 
+ limit 1;
+
+
+ select p_name 
+ from part
+ LIMIT 1;
+ 
+ 
+
+
+select Sum(ps_supplycost * ps_availqty) as total, p_name
+from partsupp
+inner join part on ps_partkey is p_partkey
+inner join supplier on s_suppkey = ps_suppkey
+inner join nation on n_nationkey = s_nationkey
+where n_name is 'UNITED STATES'
+Group BY p_name;
+--HAVING total
+--limit 1;
