b5affa000bdd319f08d8e9856f51bfa8011dc2e1
LaFrance Daniels III <55370968+LDaniels18@users.noreply.github.com>
2021-10-10 22:08:33 -0700

    13 Done

--- lab-5/test/13.sql
+++ lab-5/test/13.sql
@@ -1,1 +1,8 @@
---test
+--test - DONE
+SELECT o_orderpriority, count(DISTINCT o_orderkey)
+FROM orders
+INNER JOIN lineitem ON l_orderkey = o_orderkey
+INNER JOIN customer ON c_custkey = o_custkey
+WHERE ((strftime('%m', o_orderdate) + 2)/ 3) = 4 AND strftime('%Y', o_orderdate) is '1997' and strftime('%Y-%m-%d', l_commitdate) > strftime('%Y-%m-%d', l_receiptdate)
+GROUP BY o_orderpriority;
+
