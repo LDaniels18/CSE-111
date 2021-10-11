909e6fdad6c44a68788e9f293d091c291aa5f74a
LaFrance Daniels III <55370968+LDaniels18@users.noreply.github.com>
2021-10-10 21:26:16 -0700

    11 done

--- lab-5/test/11.sql
+++ lab-5/test/11.sql
@@ -1,4 +1,17 @@
---test
-select p_name, (l_extendedprice * (1 - l_discount)) from lineitem, part, orders
-where o_orderdate is '1996-10-02'
-group by p_name;
+--test DONE
+
+--Find the lowest value line item(s) (l extendedprice*(1-l discount)) shipped after October 2,
+--1996. Print the name of the part corresponding to these line item(s).
+
+SELECT p_name
+FROM (SELECT p_name, min(l_extendedprice * (1-l_discount))
+    FROM lineitem
+    INNER JOIN part ON p_partkey = l_partkey
+    WHERE l_shipdate > '1996-10-02');
+
+--and o_totalprice < min(;
+--order by (l_extendedprice * (1-l_discount))
+--LIMIT(SELECT count(l_extendedprice * (1-l_discount))  
+--FROM lineitem);
+
+
