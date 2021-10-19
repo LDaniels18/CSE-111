00926a12328914742d61ba511dd1231df3418575
LaFrance Daniels III <55370968+LDaniels18@users.noreply.github.com>
2021-10-19 11:53:01 -0700

    test 1

--- Lab_6/Lab 6/test/1.sql
+++ Lab_6/Lab 6/test/1.sql
@@ -1,4 +1,4 @@
-select strftime("%m", lineitem.l_shipdate) as month,
-sum(lineitem.l_quantity) from lineitem 
-where strftime("%Y", lineitem.l_shipdate) = "1995"
-group by month;
+SELECT strftime("%m", lineitem.l_shipdate) AS month, sum(lineitem.l_quantity) AS quantity 
+FROM lineitem 
+WHERE strftime("%Y", lineitem.l_shipdate) = "1995"
+GROUP BY month;
