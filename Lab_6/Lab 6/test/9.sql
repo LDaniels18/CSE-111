eea3b708699472ff3436a4ac05c9b3f1834f38f4
LaFrance Daniels III <55370968+LDaniels18@users.noreply.github.com>
2021-10-19 11:54:43 -0700

    test 9

--- Lab_6/Lab 6/test/9.sql
+++ Lab_6/Lab 6/test/9.sql
@@ -1,10 +1,11 @@
-select distinct p_name from supplier, nation as asna, region as asre, nation as amcna, region as amcre
-inner join lineitem on l_suppkey is s_suppkey
-inner join orders on o_orderkey is l_orderkey
-inner join customer on c_custkey is o_custkey
-inner join part on p_partkey is l_partkey
-where c_nationkey is amcna.n_nationkey and amcna.n_regionkey is amcre.r_regionkey
-and s_nationkey is asna.n_nationkey and asna.n_regionkey is asre.r_regionkey
-and amcre.r_name is 'AMERICA'
-and asre.r_name is 'ASIA'
-group by p_name having count(s_suppkey) is 3;
+
+SELECT DISTINCT p_name
+FROM customer, part, lineitem, orders, nation, region
+WHERE p_partkey = l_partkey AND l_orderkey = o_orderkey AND o_custkey = c_custkey AND c_nationkey = n_nationkey 
+AND n_regionkey = r_regionkey AND r_name = 'AMERICA' AND p_name 
+IN(SELECT p_name
+FROM part, lineitem, supplier, nation, region
+WHERE p_partkey = l_partkey AND l_suppkey = s_suppkey AND s_nationkey = n_nationkey AND n_regionkey = r_regionkey AND r_name = 'ASIA'
+GROUP BY p_name
+HAVING COUNT(s_suppkey) = 3)
+ORDER BY p_name;
