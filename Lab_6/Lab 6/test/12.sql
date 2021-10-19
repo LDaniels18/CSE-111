c7298fd9ed92818be71263dc8d0406b969e8de66
LaFrance Daniels III <55370968+LDaniels18@users.noreply.github.com>
2021-10-19 11:55:10 -0700

    test 12

--- Lab_6/Lab 6/test/12.sql
+++ Lab_6/Lab 6/test/12.sql
@@ -1,9 +1,8 @@
-with minPrice as(
-select n_name, sum(o_totalprice) as totalPrice from nation
-inner join customer on c_nationkey is n_nationkey
-inner join orders on o_custkey is c_custkey
-group by n_name
-order by totalPrice asc limit 1)
 
-select minPrice.n_name
-from minPrice;
+SELECT n_name
+FROM (SELECT n_name, min(TotalPrices)
+FROM (SELECT n_name, sum(o_totalprice) AS TotalPrices
+FROM nation
+INNER JOIN customer ON c_nationkey = n_nationkey
+INNER JOIN orders ON o_custkey = c_custkey
+GROUP BY n_name));
