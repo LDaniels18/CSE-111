1ded0ff435517600651eeaa351952cabf46ea20e
LaFrance Daniels III <55370968+LDaniels18@users.noreply.github.com>
2021-10-19 11:53:12 -0700

    test 2

--- Lab_6/Lab 6/test/2.sql
+++ Lab_6/Lab 6/test/2.sql
@@ -1,5 +1,7 @@
-select count(results.c_custkey) from (select customer.c_custkey from customer
-inner join orders on customer.c_custkey is orders.o_custkey
-where strftime("%Y-%m", orders.o_orderdate) is "1995-11"
-group by customer.c_custkey
-having count(customer.c_custkey) >= 3) as results;
+SELECT count(results.c_custkey)
+FROM (SELECT customer.c_custkey 
+FROM customer
+INNER JOIN orders ON customer.c_custkey = orders.o_custkey
+WHERE strftime("%Y-%m", orders.o_orderdate) = "1995-11"
+GROUP BY customer.c_custkey
+HAVING count(customer.c_custkey) >= 3) AS results;
